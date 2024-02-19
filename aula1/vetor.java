3fe2e39dd39013387b59bd54f483197c7cb27ffb
Lucas Lessa <lessalucas1102@gmail.com>
2024-02-19 13:42:43 -0300

    Aula 1 - Basico de Vetor

---
+++ aula1/vetor.java
@@ -0,0 +1,24 @@
+package aula1;
+
+public class vetor {
+
+    public static void main(String[] args) {
+
+        int[] vetor = new int[5];
+
+        vetor[0] = 1;
+        vetor[1] = 2;
+        vetor[2] = 3;
+
+        // imprimindo individualmente
+        System.out.println("vetor " + vetor[0]);
+
+        // imprimindo todos
+
+        for (int i = 0; i < vetor.length; i++) {
+
+            System.out.println("valor na posição " + i + " = " + vetor[i]);
+        }
+    }
+
+}
